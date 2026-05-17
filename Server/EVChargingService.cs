1f640351d4eead71de6c9658c636fedc65c83dc3
milosevicjovana2004-svg <milosevic.jovana2004@gmail.com>
2026-05-17 22:48:54 +0200

    Zavrsena 3. tacka

---
+++ Server/EVChargingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ServiceModel;
+using Common;
+
+namespace Server
+{
+    public class EVChargingService : IEVChargingService
+    {
+        public void StartSession(string vehicleId)
+        {
+            Console.WriteLine($"[SERVER] Sesija zapoceta za vozilo: {vehicleId}");
+        }
+
+        public void PushSample(ChargingSample sample)
+        {
+            ValidateSample(sample);
+            Console.WriteLine($"[SERVER] Primljen red {sample.RowIndex} za vozilo {sample.VehicleId} - prenos u toku...");
+        }
+
+        public void EndSession(string vehicleId)
+        {
+            Console.WriteLine($"[SERVER] Sesija zavrsena za vozilo: {vehicleId}");
+            Console.WriteLine($"[SERVER] Prenos zavrsen.");
+        }
+
+        private void ValidateSample(ChargingSample sample)
+        {
+            if (string.IsNullOrEmpty(sample.Timestamp))
+            {
+                throw new FaultException<string>(
+                    "Neispravan red: Timestamp je prazan.",
+                    new FaultReason("Validacija nije prosla"));
+            }
+
+            if (sample.VoltageAvg <= 0)
+            {
+                throw new FaultException<string>(
+                    $"Neispravan red {sample.RowIndex}: Napon mora biti veci od 0.",
+                    new FaultReason("Validacija nije prosla"));
+            }
+
+            if (sample.FrequencyAvg <= 0)
+            {
+                throw new FaultException<string>(
+                    $"Neispravan red {sample.RowIndex}: Frekvencija mora biti veca od 0.",
+                    new FaultReason("Validacija nije prosla"));
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace Server
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(EVChargingService));
+            host.Open();
+            Console.WriteLine("[SERVER] Servis je pokrenut. Cekam klijente...");
+            Console.WriteLine("[SERVER] Pritisni Enter za gasenje.");
+            Console.ReadLine();
+            host.Close();
         }
     }
 }
