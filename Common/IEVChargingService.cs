d1b2e045b00994bce16ba53e27ee5e39888161ef
Djukica111 <vulenced@gmail.com>
2026-05-17 22:28:27 +0200

    2. tacka gotova

---
+++ Common/IEVChargingService.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IEVChargingService
+    {
+        [OperationContract]
+        [FaultContract(typeof(string))]
+        void StartSession(string vehicleId);
+
+        [OperationContract]
+        [FaultContract(typeof(string))]
+        void PushSample(ChargingSample sample);
+
+        [OperationContract]
+        [FaultContract(typeof(string))]
+        void EndSession(string vehicleId);
+    }
+}
