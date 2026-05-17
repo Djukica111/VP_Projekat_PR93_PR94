45528cdceac4330d07f632cf8a348ee1b1a01f6b
Djukica111 <vulenced@gmail.com>
2026-05-18 00:12:42 +0200

    10 zadatak gotov: Analitika: frekvencija i stabilnost sistema

--- Common/TransferEventArgs.cs
+++ Common/TransferEventArgs.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Common
 {
-    internal class TransferEventArgs
+    public class TransferStartedEventArgs : EventArgs
     {
+        public string VehicleId { get; }
+        public DateTime VremePocetka { get; }
+
+        public TransferStartedEventArgs(string vehicleId)
+        {
+            VehicleId = vehicleId;
+            VremePocetka = DateTime.Now;
+        }
+    }
+
+    public class SampleReceivedEventArgs : EventArgs
+    {
+        public string VehicleId { get; }
+        public int RowIndex { get; }
+        public int UkupnoPrimljeno { get; }
+
+        public SampleReceivedEventArgs(string vehicleId, int rowIndex, int ukupno)
+        {
+            VehicleId = vehicleId;
+            RowIndex = rowIndex;
+            UkupnoPrimljeno = ukupno;
+        }
+    }
+
+    public class TransferCompletedEventArgs : EventArgs
+    {
+        public string VehicleId { get; }
+        public int UkupnoRedova { get; }
+        public DateTime VremeKraja { get; }
+
+        public TransferCompletedEventArgs(string vehicleId, int ukupno)
+        {
+            VehicleId = vehicleId;
+            UkupnoRedova = ukupno;
+            VremeKraja = DateTime.Now;
+        }
+    }
+
+    public class WarningRaisedEventArgs : EventArgs
+    {
+        public string VehicleId { get; }
+        public int RowIndex { get; }
+        public string TipUpozorenja { get; }
+        public string Poruka { get; }
+        public double TrenutnaVrednost { get; }
+
+        public WarningRaisedEventArgs(string vehicleId, int rowIndex,
+            string tipUpozorenja, string poruka, double trenutnaVrednost)
+        {
+            VehicleId = vehicleId;
+            RowIndex = rowIndex;
+            TipUpozorenja = tipUpozorenja;
+            Poruka = poruka;
+            TrenutnaVrednost = trenutnaVrednost;
+        }
     }
 }
---
+++ Server/FrequencyAnalyzer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Configuration;
+using Common;
+
+namespace Server
+{
+    public class FrequencyAnalyzer
+    {
+        private const double NOMINALNA_FREKVENCIJA = 50.0;
+        private const double DOZVOLJENO_ODSTUPANJE = 0.6;
+
+        private double _prethodniFreqMin = -1;
+        private double _prethodniFreqMax = -1;
+        private double _spikePrag;
+
+        public TransferEventPublisher Publisher { get; set; }
+
+        public FrequencyAnalyzer(TransferEventPublisher publisher)
+        {
+            Publisher = publisher;
+
+            string vrednost = ConfigurationManager
+                .AppSettings["FrequencySpikeThreshold"];
+            if (!double.TryParse(vrednost, out _spikePrag))
+                _spikePrag = 1.0;
+        }
+
+        public void AnalizirajUzorak(ChargingSample sample)
+        {
+            ProveriOdstupanje(sample);
+            ProveriNagliSkok(sample);
+
+            // Zapamti vrednosti za sledeci red
+            _prethodniFreqMin = sample.FrequencyMin;
+            _prethodniFreqMax = sample.FrequencyMax;
+        }
+
+        private void ProveriOdstupanje(ChargingSample sample)
+        {
+            double odstupanje = Math.Abs(
+                sample.FrequencyAvg - NOMINALNA_FREKVENCIJA);
+
+            if (odstupanje > DOZVOLJENO_ODSTUPANJE)
+            {
+                Publisher.GeneriširWarning(
+                    sample.VehicleId,
+                    sample.RowIndex,
+                    "FrequencyDeviationWarning",
+                    $"Frequency Avg ({sample.FrequencyAvg:F3} Hz) odstupa za " +
+                    $"{odstupanje:F3} Hz od nominalne vrednosti " +
+                    $"({NOMINALNA_FREKVENCIJA} Hz). " +
+                    $"Dozvoljeno odstupanje: ±{DOZVOLJENO_ODSTUPANJE} Hz",
+                    sample.FrequencyAvg);
+            }
+        }
+
+        private void ProveriNagliSkok(ChargingSample sample)
+        {
+            // Preskoci prvi red jer nema prethodnih vrednosti
+            if (_prethodniFreqMin < 0 || _prethodniFreqMax < 0)
+                return;
+
+            double deltaMin = Math.Abs(sample.FrequencyMin - _prethodniFreqMin);
+            double deltaMax = Math.Abs(sample.FrequencyMax - _prethodniFreqMax);
+
+            if (deltaMin > _spikePrag)
+            {
+                Publisher.GeneriširWarning(
+                    sample.VehicleId,
+                    sample.RowIndex,
+                    "FrequencySpike",
+                    $"Nagli skok Frequency Min: " +
+                    $"prethodni={_prethodniFreqMin:F3} Hz, " +
+                    $"trenutni={sample.FrequencyMin:F3} Hz, " +
+                    $"delta={deltaMin:F3} Hz (prag={_spikePrag:F3} Hz)",
+                    deltaMin);
+            }
+
+            if (deltaMax > _spikePrag)
+            {
+                Publisher.GeneriširWarning(
+                    sample.VehicleId,
+                    sample.RowIndex,
+                    "FrequencySpike",
+                    $"Nagli skok Frequency Max: " +
+                    $"prethodni={_prethodniFreqMax:F3} Hz, " +
+                    $"trenutni={sample.FrequencyMax:F3} Hz, " +
+                    $"delta={deltaMax:F3} Hz (prag={_spikePrag:F3} Hz)",
+                    deltaMax);
+            }
+        }
+
+        public void ResetujStanje()
+        {
+            _prethodniFreqMin = -1;
+            _prethodniFreqMax = -1;
+        }
+    }
+}
